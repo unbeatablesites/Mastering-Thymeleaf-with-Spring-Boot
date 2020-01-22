97239b0e11fe6d7ffee45ccea2319879b7cb8228
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-22 14:59:21 -0500

    Controller mapped product page

--- ThymeLeaf/src/main/java/com/Frank_Uzoka/ThymeLeaf/controller/ProductController.java
+++ ThymeLeaf/src/main/java/com/Frank_Uzoka/ThymeLeaf/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.Frank_Uzoka.ThymeLeaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class ProductController {
