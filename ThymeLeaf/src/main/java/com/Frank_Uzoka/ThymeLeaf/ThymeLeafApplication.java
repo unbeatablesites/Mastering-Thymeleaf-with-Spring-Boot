26bbdbffb686f164da304895ec9da7e7611aec13
Frank Uzoka <Frankuzoka@hotmail.com>
2020-01-22 12:14:36 -0500

    Adding dependencies

---
+++ ThymeLeaf/src/main/java/com/Frank_Uzoka/ThymeLeaf/ThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.Frank_Uzoka.ThymeLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafApplication.class, args);
+	}
+
+}
---
+++ ThymeLeaf/src/test/java/com/Frank_Uzoka/ThymeLeaf/ThymeLeafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Frank_Uzoka.ThymeLeaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
